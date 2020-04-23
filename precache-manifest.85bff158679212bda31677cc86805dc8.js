c66cd4fac9dba827cc978c07cf008e579d8472a3
Filip Swacha <filip.swacha@gmail.com>
2020-04-23 16:23:41 +0200

    Updates

---
+++ precache-manifest.85bff158679212bda31677cc86805dc8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6723fdaca41b3109eb74f4b2cd420877",
+    "url": "/travel-destination-keeper/index.html"
+  },
+  {
+    "revision": "a70f97e13b7219e7cf36",
+    "url": "/travel-destination-keeper/static/css/main.4c7d8acd.chunk.css"
+  },
+  {
+    "revision": "4dd93aee620c5861dc79",
+    "url": "/travel-destination-keeper/static/js/2.8fdffbf2.chunk.js"
+  },
+  {
+    "revision": "a70f97e13b7219e7cf36",
+    "url": "/travel-destination-keeper/static/js/main.a52d36b8.chunk.js"
+  },
+  {
+    "revision": "a45eac2b9ae3a4ec209f",
+    "url": "/travel-destination-keeper/static/js/runtime~main.8bdf98b3.js"
+  }
+]);
